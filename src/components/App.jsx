8460d49a5e3b9be059de05287d3640ab4d8c9b1b
IIROFESSOR-X <IIROFESSOR@gmail.com>
2024-03-05 19:19:12 +0200

    fix 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import { Profile } from './Profile/Profile';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransationHistory/TransationHistory';
+import { Profile } from "./Profile/Profile";
+import { FriendList } from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 import userData from '../userData.json';
 import friends from '../friends.json';
 import transactions from '../transation.json';
