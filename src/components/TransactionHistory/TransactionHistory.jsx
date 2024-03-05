1a04e78c08caa112b3ff3ccbabfea93c4ff62e2c
IIROFESSOR-X <IIROFESSOR@gmail.com>
2024-03-05 19:33:48 +0200

    final fix

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,28 +1,27 @@
-import css from './TransactionHistory.module.css'
+import css from './TransactionHistory.module.css';
 
-const TransactionHistory = ({ items }) => {
-    return (
-      <>
-        <table className={css.container}>
-  <thead>
-    <tr className={css.title}>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
-  <tbody className={css.item}>
-  {items.map(item => (
-    <tr className={css.color} key={item.id}>
-      <td className={css.text}>{item.type}</td>
-      <td>{item.amount}</td>
-      <td>{item.currency}</td>
-    </tr>
-   ))}
-  </tbody>
-</table>
-      </>
-    );
-  };
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transaction}>
+      <thead className={css.head}>
+        <tr>
+          <th className={css.th}>Type</th>
+          <th className={css.th}>Amount</th>
+          <th className={css.th}>Currency</th>
+        </tr>
+      </thead>
 
-  export default TransactionHistory;
+      <tbody>
+        {items.map(transaction => {
+          return (
+            <tr className={css.tr} key={transaction.id}>
+              <td className={css.td}>{transaction.type}</td>
+              <td className={css.td}>{transaction.amount}</td>
+              <td className={css.td}>{transaction.currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
